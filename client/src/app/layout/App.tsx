d0ead140d921ca37c7473cc452f54728df7ea13f
TGS Berin <berinmadzak@gmail.com>
2024-04-02 21:13:55 +0200

    feat: implement dark mode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,16 +1,26 @@
-import { Container, CssBaseline } from "@mui/material";
+import { Container, CssBaseline, ThemeProvider, createTheme, useTheme } from "@mui/material";
 import Library from "../../features/library/Library";
 import Header from "./Header";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const themeStyle = darkMode ? 'dark' : 'light';
+
+  const theme = createTheme({
+    palette: {
+      mode: themeStyle
+    }
+  });
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
       <Container sx={{mt: 10}}>
         <Library />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,13 +1,24 @@
 import { ShoppingCart } from "@mui/icons-material";
-import { AppBar, Badge, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, Badge, Box, IconButton, Theme, Toolbar, Typography } from "@mui/material";
+import { useMemo, useState } from "react";
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
-export default function Header()
+interface Props {
+    darkMode: boolean;
+    setDarkMode: (value: boolean) => void;
+}
+
+export default function Header({darkMode, setDarkMode}: Props)
 {
     return (
         <AppBar position="fixed">
             <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
                 <Box display='flex' alignItems='center'>
                         <Typography variant="h3">Online Bookstore</Typography>
+                        <IconButton sx={{ml: 1}} onClick={() => setDarkMode(!darkMode)} color='inherit'>
+                            {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+                        </IconButton>
                 </Box>
                 <Box display='flex' alignItems='center'>
                     <IconButton size='large' sx={{mr: 2}}>
